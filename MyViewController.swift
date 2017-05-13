bf12befbd4720bbcceeb83cabb27c60491e67e66
JIAWEI CHEN <jiaweichen@JIAWEIs-MacBook-Pro.local>
2017-05-13 09:35:28 -0700

    view & vc

---
+++ MyViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MyViewController.swift
+//  codechallenge
+//
+//  Created by JIAWEI CHEN on 5/13/17.
+//  Copyright © 2017 John. All rights reserved.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func takePicture(_ sender: Any) {
+    }
+   
+}
